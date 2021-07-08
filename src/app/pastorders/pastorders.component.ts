a110501869cb24d854d6e1ff7146e3ed08ed0ab6
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-08 09:22:45 +0530

    connect db

--- src/app/pastorders/pastorders.component.ts
+++ src/app/pastorders/pastorders.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
+//1. Create an Interface for object type with variables
+export interface Pastorders{
+  id: string;
+  date: string;
+  name: string;
+  address: string;
+  phone: string;
+  // order: string;
+  price: number;
+
+}
+
 @Component({
   selector: 'app-pastorders',
   templateUrl: './pastorders.component.html',
